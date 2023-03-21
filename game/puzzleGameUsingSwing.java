21b931b65632e3e04d0e51634e7473902556f676
Jaipal001 <105307427+BhJaipal@users.noreply.github.com>
2023-03-21 23:13:00 +0530

    Update puzzleGameUsingSwing.java

--- game/puzzleGameUsingSwing.java
+++ game/puzzleGameUsingSwing.java
@@ -40,102 +40,95 @@ public class puzzle extends JFrame implements ActionListener{
         b9.addActionListener(this);  
         next.addActionListener(this);  
         
-next.setBackground(Color.black);  
-next.setForeground(Color.green);  
-setSize(250,300);  
-setLayout(null);  
-setVisible(true);  
-setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
-}//end of constructor  
+        next.setBackground(Color.black);  
+        next.setForeground(Color.green);  
+        setSize(250,300);  
+        setLayout(null);  
+        setVisible(true);  
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
+    }//end of constructor  
   
-public void actionPerformed(ActionEvent e){  
-if(e.getSource()==next){  
-String s=b4.getLabel();  
-b4.setLabel(b9.getLabel());  
-b9.setLabel(s);  
-s=b1.getLabel();  
-b1.setLabel(b5.getLabel());  
-b5.setLabel(s);  
-s=b2.getLabel();  
-b2.setLabel(b7.getLabel());  
-b7.setLabel(s);  
-}  
-if(e.getSource()==b1){  
-String s=b1.getLabel();  
-if(b2.getLabel().equals(" ")){ b2.setLabel(s); b1.setLabel(" ");}  
-else if(b4.getLabel().equals(" ")){ b4.setLabel(s); b1.setLabel(" ");}  
- }//end of if  
+    public void actionPerformed(ActionEvent e){  
+        if(e.getSource()==next){  
+            String s=b4.getLabel();  
+            b4.setLabel(b9.getLabel());  
+            b9.setLabel(s);  
+            s=b1.getLabel();  
+            b1.setLabel(b5.getLabel());  
+            b5.setLabel(s);  
+            s=b2.getLabel();  
+            b2.setLabel(b7.getLabel());  
+            b7.setLabel(s);  
+        }  
+        
+        if(e.getSource()==b1){  
+            String s=b1.getLabel();  
+            if(b2.getLabel().equals(" ")){ b2.setLabel(s); b1.setLabel(" ");}  
+            else if(b4.getLabel().equals(" ")){ b4.setLabel(s); b1.setLabel(" ");}  
+        }//end of if  
+        
+        if(e.getSource()==b3){  
+            String s=b3.getLabel();  
+            if(b2.getLabel().equals(" ")){ b2.setLabel(s); b3.setLabel(" ");}  
+            else if(b6.getLabel().equals(" ")){ b6.setLabel(s); b3.setLabel(" ");}  
+        }//end of if  
+        
+        if(e.getSource()==b2){  
+            String s=b2.getLabel();  
+            if(b1.getLabel().equals(" ")){ b1.setLabel(s); b2.setLabel(" ");}  
+            else if(b3.getLabel().equals(" ")){ b3.setLabel(s); b2.setLabel(" ");}  
+            else if(b5.getLabel().equals(" ")){ b5.setLabel(s); b2.setLabel(" ");}  
+        }//end of if  
+        
+        if(e.getSource()==b4){  
+            String s=b4.getLabel();  
+            if(b1.getLabel().equals(" ")){ b1.setLabel(s); b4.setLabel(" ");}  
+            else if(b7.getLabel().equals(" ")){ b7.setLabel(s); b4.setLabel(" ");}  
+            else if(b5.getLabel().equals(" ")){ b5.setLabel(s); b4.setLabel(" ");}  
+        }//end of if  
+        
+        if(e.getSource()==b5){ 
+            String s=b5.getLabel();  
+            if(b2.getLabel().equals(" ")){ b2.setLabel(s); b5.setLabel(" ");}  
+            else if(b4.getLabel().equals(" ")){ b4.setLabel(s); b5.setLabel(" ");}  
+            else if(b6.getLabel().equals(" ")){ b6.setLabel(s); b5.setLabel(" ");}  
+            else if(b8.getLabel().equals(" ")){ b8.setLabel(s); b5.setLabel(" ");}  
+        }//end of if  
+        
+        if(e.getSource()==b6){  
+            String s=b6.getLabel();  
+            if(b9.getLabel().equals(" ")){ b9.setLabel(s); b6.setLabel(" ");}  
+            else if(b3.getLabel().equals(" ")){ b3.setLabel(s); b6.setLabel(" ");}  
+            else if(b5.getLabel().equals(" ")){ b5.setLabel(s); b6.setLabel(" ");}  
+        }//end of if  
+        
+        if(e.getSource()==b7){  
+            String s=b7.getLabel();  
+            if(b4.getLabel().equals(" ")){ b4.setLabel(s); b7.setLabel(" ");}  
+            else if(b8.getLabel().equals(" ")){ b8.setLabel(s); b7.setLabel(" ");}  
+        }//end of if  
+
+        if(e.getSource()==b8){  
+            String s=b8.getLabel();  
+            if(b7.getLabel().equals(" ")){ b7.setLabel(s); b8.setLabel(" ");}  
+            else if(b9.getLabel().equals(" ")){ b9.setLabel(s); b8.setLabel(" ");}  
+            else if(b5.getLabel().equals(" ")){ b5.setLabel(s); b8.setLabel(" ");}  
+        }//end of if  
+
+        if(e.getSource()==b9){  
+            String s=b9.getLabel();  
+            if(b6.getLabel().equals(" ")){ b6.setLabel(s); b9.setLabel(" ");}  
+            else if(b8.getLabel().equals(" ")){ b8.setLabel(s); b9.setLabel(" ");}  
   
-if(e.getSource()==b3){  
-String s=b3.getLabel();  
-if(b2.getLabel().equals(" ")){ b2.setLabel(s); b3.setLabel(" ");}  
-else if(b6.getLabel().equals(" ")){ b6.setLabel(s); b3.setLabel(" ");}  
- }//end of if  
-  
-if(e.getSource()==b2){  
-String s=b2.getLabel();  
-if(b1.getLabel().equals(" ")){ b1.setLabel(s); b2.setLabel(" ");}  
-else if(b3.getLabel().equals(" ")){ b3.setLabel(s); b2.setLabel(" ");}  
-else if(b5.getLabel().equals(" ")){ b5.setLabel(s); b2.setLabel(" ");}  
- }//end of if  
-  
-if(e.getSource()==b4){  
-String s=b4.getLabel();  
-if(b1.getLabel().equals(" ")){ b1.setLabel(s); b4.setLabel(" ");}  
-else if(b7.getLabel().equals(" ")){ b7.setLabel(s); b4.setLabel(" ");}  
-else if(b5.getLabel().equals(" ")){ b5.setLabel(s); b4.setLabel(" ");}  
- }//end of if  
-  
-if(e.getSource()==b5){  
-String s=b5.getLabel();  
-if(b2.getLabel().equals(" ")){ b2.setLabel(s); b5.setLabel(" ");}  
-else if(b4.getLabel().equals(" ")){ b4.setLabel(s); b5.setLabel(" ");}  
-else if(b6.getLabel().equals(" ")){ b6.setLabel(s); b5.setLabel(" ");}  
-else if(b8.getLabel().equals(" ")){ b8.setLabel(s); b5.setLabel(" ");}  
- }//end of if  
-  
-if(e.getSource()==b6){  
-  
-String s=b6.getLabel();  
-if(b9.getLabel().equals(" ")){ b9.setLabel(s); b6.setLabel(" ");}  
-else if(b3.getLabel().equals(" ")){ b3.setLabel(s); b6.setLabel(" ");}  
-else if(b5.getLabel().equals(" ")){ b5.setLabel(s); b6.setLabel(" ");}  
-  
- }//end of if  
-  
-if(e.getSource()==b7){  
-String s=b7.getLabel();  
-if(b4.getLabel().equals(" ")){ b4.setLabel(s); b7.setLabel(" ");}  
-else if(b8.getLabel().equals(" ")){ b8.setLabel(s); b7.setLabel(" ");}  
-  
- }//end of if  
-  
-if(e.getSource()==b8){  
-String s=b8.getLabel();  
-if(b7.getLabel().equals(" ")){ b7.setLabel(s); b8.setLabel(" ");}  
-else if(b9.getLabel().equals(" ")){ b9.setLabel(s); b8.setLabel(" ");}  
-else if(b5.getLabel().equals(" ")){ b5.setLabel(s); b8.setLabel(" ");}  
-  
- }//end of if  
-  
-if(e.getSource()==b9){  
-String s=b9.getLabel();  
-if(b6.getLabel().equals(" ")){ b6.setLabel(s); b9.setLabel(" ");}  
-else if(b8.getLabel().equals(" ")){ b8.setLabel(s); b9.setLabel(" ");}  
-  
-if(b1.getLabel().equals("1")&&b2.getLabel().equals("2")&&b3.getLabel()  
-.equals("3")&&b4.getLabel().equals("4")&&b5.getLabel().equals("5")  
-&&b6.getLabel().equals("6")&&b7.getLabel().equals("7")&&b8.getLabel()  
-.equals("8")&&b9.getLabel().equals(" ")){   
-JOptionPane.showMessageDialog(puzzle.this,"!!!you won!!!");  
-}  
- }//end of if  
-  
-}//end of actionPerformed  
+            if(b1.getLabel().equals("1") && b2.getLabel().equals("2") && b3.getLabel().equals("3") && b4.getLabel().equals("4") && b5.getLabel().equals("5")  
+               && b6.getLabel().equals("6") && b7.getLabel().equals("7") && b8.getLabel().equals("8") && b9.getLabel().equals(" ")){   
+                JOptionPane.showMessageDialog(puzzle.this,"!!!you won!!!");  
+            }  
+        }//end of if  
+    }//end of actionPerformed  
    
-  
-public static void main(String[] args){  
-new puzzle();  
-}//end of main  
-  
+
+    public static void main(String[] args){  
+        new puzzle();  
+    }//end of main  
 }//end of class  
